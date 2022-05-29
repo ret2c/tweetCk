b58b08b366a40f66fab7dc67ae3368aad30c187a
connor <connorskastner@gmail.com>
2022-05-29 18:10:12 -0500

    Clean up HTML code within PHP lines

--- tweet.php
+++ tweet.php
@@ -19,13 +19,21 @@ if(!empty($_REQUEST['submission']) && !empty($_REQUEST['uname'])) {
     if($check == true) {
         unset($submission); unset($uname);
         ?>
-            <head><title>tweet</title><style>body {padding-top: 200px;padding-left: 200px;padding-right:200px;background-color: black;color: white;} div.b {word-wrap: break-word;color: red;}a {color: white;}a:hover {background-color: red;}a:visited {text-decoration: none;color: white;}</style></head>
+            <html>
+            <head>
+                <title>tweet</title>
+                <style>
+                /* CSS Cleanup */
+                body {padding-top: 200px;padding-left: 200px;padding-right:200px;background-color: black;color: white;} div.b {word-wrap: break-word;color: red;}a {color: white;}a:hover {background-color: red;}a:visited {text-decoration: none;color: white;}
+                </style>
+            </head>
             <body>
                 <center>
                     <!-- Recovery -->
                     <p><div class="b"><b>Your submission contains a banned word.</div></b><br><a href="index.php">Try Again</a></p>
                 </center>
             </body>
+            </html>
             <?php
     } else {
         // Post to Twitter with Abraham\TwitterOAuth
@@ -40,18 +48,31 @@ if(!empty($_REQUEST['submission']) && !empty($_REQUEST['uname'])) {
         if($connection->getLastHttpCode() != 200) {
             // HTTP Error
             ?>
-            <head><title>Unexpected Error</title><style>body {padding-top: 200px;padding-left: 200px;padding-right:200px;background-color: black;color: white;} div.b {word-wrap: break-word;color: red;}a {color: white;}a:hover {background-color: red;}a:visited {text-decoration: none;color: white;}</style></head>
+            <html>
+            <head>
+                <title>Unexpected Error</title>
+                <style>body {padding-top: 200px;padding-left: 200px;padding-right:200px;background-color: black;color: white;} div.b {word-wrap: break-word;color: red;}a {color: white;}a:hover {background-color: red;}a:visited {text-decoration: none;color: white;}
+            </style>
+            </head>
             <body>
                 <center>
                     <!-- Recovery -->
-                    <p>An unrecoverable error has occured.<br>If you're a regular user, please DM me on <a href="https://twitter.com:443/">Twitter</a> with the following error code:<br><div class="b">ERROR NO. <?php echo base64_encode($result->errors[0]->message);?></div><br><br><?php die("\nPHP terminated."); unset($submission); unset($uname);?></p><br><br>
+                    <p>An unrecoverable error has occured.<br>If you're a regular user, please DM me on <a href="https://twitter.com:443/">Twitter</a> with the following error code:<br>
+                    <div class="b">ERROR NO. <?php echo base64_encode($result->errors[0]->message);?></div><br><br>
+                    <?php die("\nPHP terminated."); unset($submission); unset($uname);?></p><br><br>
                 </center>
             </body>
+            </html>
             <?php
         } else {
             // Success
             ?>
-            <head><title>tweet</title><style>body {;padding-top: 200px;padding-left: 200px;padding-right:200px;background-color: black;color: white;}div.a {color: #32CD32}a {color: white;}a:hover {background-color: red;}a:visited {text-decoration: none;color: white;}</style></head>
+            <html>
+            <head>
+                <title>tweet</title>
+                <style>body {;padding-top: 200px;padding-left: 200px;padding-right:200px;background-color: black;color: white;}div.a {color: #32CD32}a {color: white;}a:hover {background-color: red;}a:visited {text-decoration: none;color: white;}
+            </style>
+            </head>
             <body>
             <center>
                 <p><div class="a"><b>Successfully posted to Twitter!</div><br><a href="https://twitter.com/">Checkout your tweet here</a></b></p>
@@ -59,6 +80,7 @@ if(!empty($_REQUEST['submission']) && !empty($_REQUEST['uname'])) {
                 <p><a href="index.php">Post Another?</a></p>
             </center>
             </body>
+            </html>
             <?php die(); ?>
             <?php
         }
@@ -76,15 +98,13 @@ if(!empty($_REQUEST['submission']) && !empty($_REQUEST['uname'])) {
 <body>
 <center>
     <h3>##### Enter Your Tweet #####</h3>
-    <p>todo: make website look pretty :)</p>
-
     <form method="POST">
-    <p><b>Input submission:</b><br>max character length: <b>262</b></p>
-    <textarea type="text" rows="5" cols="60" name="submission" maxlength="262" required></textarea>
-    <p><b>Input Twitter username:</b><br>no special characters allowed</p>
-    <input type="text" name="uname" value="" placeholder="twitterUsername" pattern="[a-zA-Z0-9]+" minlength="4" maxlength="15" required>
-    <br><br>
-    <input type="submit" name="submit" value="Submit">
+        <p><b>Input submission:</b><br>max character length: <b>262</b></p>
+        <textarea type="text" rows="5" cols="60" name="submission" maxlength="262" required></textarea>
+        <p><b>Input Twitter username:</b><br>no special characters allowed</p>
+        <input type="text" name="uname" value="" placeholder="twitterUsername" pattern="[a-zA-Z0-9]+" minlength="4" maxlength="15" required>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
     </form>
 </center>
 <?php
